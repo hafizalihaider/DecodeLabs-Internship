679941fb54a2a2e1b86de7a177320c4bea29be5a
Ali <160371439+hafizalihaider@users.noreply.github.com>
2026-07-31 15:03:56 +0500

    Sample Email Add

--- Project-3-Phishing-Awareness-Analysis/phishing-awareness-analysis.py
+++ Project-3-Phishing-Awareness-Analysis/phishing-awareness-analysis.py
@@ -1,1 +1,25 @@
-print("Hello")
+print("="*70)
+print(f"{'Phishing Awareness Analysis':^70}")
+print("="*70)
+
+while True:
+
+    print("\nStart Menu")
+    print("-"*12)
+    print("\n1. Analyze Email")
+    print("2. Exit")
+
+    try:
+        choice = int(input("\nEnter your choice: "))
+
+        if choice == 1:
+                pass
+
+        elif choice == 2:
+                print("\nThank you for using Phishing Awareness Analyzer.")
+                break
+        else:
+            print("\nPlease choose 1 or 2.")
+
+    except ValueError:
+            print("\nPlease enter a numeric choice.")
