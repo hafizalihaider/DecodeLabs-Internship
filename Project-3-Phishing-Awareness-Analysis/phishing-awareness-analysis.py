5e3bb4e200f4dedde3b0f7a1edfc54156afedd8e
Ali <160371439+hafizalihaider@users.noreply.github.com>
2026-08-01 03:01:55 +0500

    New

--- Project-3-Phishing-Awareness-Analysis/phishing-awareness-analysis.py
+++ Project-3-Phishing-Awareness-Analysis/phishing-awareness-analysis.py
@@ -18,7 +18,42 @@ def main_menu():
         print("\n1. Analyze Demo Email")
         print("2. Analyze Custom Email")
         print("3. View Analysis Reports")
-        print("4. Exit")
+        print("0. Exit")
+
+        while True:
+
+                choice = input("\nEnter your choice: ")
+
+                if choice.isdigit():
+                       choice = int(choice)
+                       if choice == 1:
+                            demo_email_menu()
+
+                       elif choice == 2:
+                           analyze_custom_email()
+
+                       elif choice == 3:
+                            view_reports()
+
+                       elif choice == 0:
+                            print("\nThank you for using Phishing Awareness Analyzer.")
+                            break
+                       else:
+                            print("\n[!] Invalid choice. Please select an option from 0-3.")
+
+                else:
+                        print("\n[!] Invalid choice. Please select an option from 0-3.")
+
+def demo_email_menu():
+
+        print("-" * 70)
+        print(f"{'Demo Email Analyzer':^70}")
+        print("-" * 70)
+
+        print("\n1. Analyze One Email")
+        print("2. Analyze Multiple Emails")
+        print("3. Analyze all Emails")
+        print("0. Exit")
 
         while True:
 
@@ -31,21 +66,20 @@ def main_menu():
 
 
                        elif choice == 2:
-                           analyze_custom_email()
-
+                           get_demo_emails()
 
                        elif choice == 3:
-                            view_reports()
+                            print("Analyze all emails added soon.")
 
-                       elif choice == 4:
-                            print("\nThank you for using Phishing Awareness Analyzer.")
+                       elif choice == 0:
                             break
                        else:
-                            print("\n[!] Invalid choice. Please select an option from 1-4.")
+                            print("\n[!] Invalid choice. Please select an option from 0-3.")
 
                 else:
-                        print("\n[!] Invalid choice. Please select an option from 1-4.")
-    
+                        print("\n[!] Invalid choice. Please select an option from 0-3.")
+
+
 def get_demo_emails():
 
     print("="*70)
@@ -62,13 +96,12 @@ def get_demo_emails():
         if file.endswith(".txt"):
               email_list.append(file)
 
+    email_list.sort(key = lambda file: int(file.split("-")[1]))
     print(f"\n{len(email_list)} email(s) found!\n")
 
     for number,files in enumerate(email_list, 1):
           print(f"[{number:02}] {files}")
           
-
-    print(f"[{len(email_list) + 1}] All of them")
     print("\n[00] Back")
     
 
@@ -86,20 +119,11 @@ def get_demo_emails():
                 if choice >= 1 and choice <= len(email_list):
                       print("Selected:", email_list[choice - 1])
 
-                elif choice == len(email_list) + 1:
-                      print("Analyzing all emails...")
-
                 else:
                       print(f"\n[!] Invalid selection. Please enter a number between 1-{len(email_list)}, {len(email_list)+1} for all emails, or [00] to go back.")
 
         else:
               print(f"\n[!] Invalid selection. Please enter a number between 1-{len(email_list)}, {len(email_list)+1} for all emails, or [00] to go back.")
-
-def analyze_demo_email():
-    print("\n[Analyze Demo Email]")
-    print("Feature will be added soon.")
-
-
 
 def analyze_custom_email():
     print("\n[Analyze Custom Email]")
